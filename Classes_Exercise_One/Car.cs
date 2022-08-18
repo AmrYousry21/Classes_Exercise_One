fa8b755ef6698403b3454f501ccdc7465107eb87
Amr Yousry <amryousry19@gmail.com>
2022-08-18 12:30:09 -0400

    Classes Exercise One

---
+++ Classes_Exercise_One/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace Classes_Excercise_One // Note: actual namespace depends on the project name.
+{
+    public class Car 
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
---
+++ Classes_Exercise_One/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_Excercise_One // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car
+            {
+                Make = "Honda",
+                Model = "HRV",
+                Year = 2015
+            };
+
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine($"Year: {myCar.Year}");
+        }
+    }
+}
